b9e45a0ada6ea7aae8869460f1ddec3fad3fbec9
Marissa Phul <mphul044@uottawa.ca>
2018-10-05 14:37:03 -0400

    added dropdown menu with android spinner object

--- app/src/main/java/com/example/maryjoe/homerepairservice/DisplayMessageActivity.java
+++ app/src/main/java/com/example/maryjoe/homerepairservice/DisplayMessageActivity.java
@@ -3,6 +3,8 @@ package com.example.maryjoe.homerepairservice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -17,6 +19,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView3);
-        textView.setText(message);}
+        TextView textView = findViewById(R.id.signUp);
+        textView.setText(message);
+
+        Spinner accountChoice = (Spinner) findViewById(R.id.dropdownMenu);
+
+        ArrayAdapter<String> chooseAccountType= new ArrayAdapter<String>(DisplayMessageActivity.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.accountTypes));
+        chooseAccountType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountChoice.setAdapter(chooseAccountType);
+    }
+
+
 }
