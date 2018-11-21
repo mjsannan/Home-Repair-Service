1952a3e887eae2f5239af7038f32e21ae63a2cb1
MaryJoe <msann027@uottawa.ca>
2018-11-20 23:53:16 -0500

    DEliv 3

---
+++ README.txt
@@ -0,0 +1,30 @@
+Group Names:
+
+
+Marie Joe Sannan
+
+Marissa Phul
+
+Margaret Ajibola
+
+Omade Okwor
+
+
+
+Repository Link:
+
+
+https://github.com/msann027/Home-Repair-Service.git
+
+
+
+Admin Login:
+
+
+Username: admin_1
+
+Email: admintest@yahoo.com
+
+Password: password
+
+Tested APK on Samsung Mobile Model SM-J320W8
---
+++ androidFiles/app/src/main/java/com/example/maryjoe/segapp/WelcomePage.java
@@ -0,0 +1,48 @@
+package com.example.maryjoe.segapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(SignIn.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView nameTextField = findViewById(R.id.nameTextField);
+        nameTextField.setText(SignUp.getNameOfUser());
+
+        TextView loggedTextView = findViewById(R.id.loggedTextView);
+        loggedTextView.setText(SignUp.getAccountType());
+    }
+
+    public void onClick(View v){
+       continueButton(v);
+    }
+
+    SignUp signUp = new SignUp();
+
+    public void continueButton(View view){
+        Intent intent = new Intent (WelcomePage.this, ServiceOptions.class);
+        startActivity(intent);
+        /*
+        if ((signUp.getAccountType()).equals("Service Provider")){
+            Intent intent = new Intent(WelcomePage.this, ServiceOptions.class);
+            startActivity(intent);
+        }
+        else if ((signUp.getAccountType()).equals("Homeowner")){
+            //Do nothing for now
+        }
+    */
+    }
+
+}
