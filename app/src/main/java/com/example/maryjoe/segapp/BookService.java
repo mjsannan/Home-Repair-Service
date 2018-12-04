d22a20f6659072ed51240b4e7a2f4f6d191398a0
MaryJoe <msann027@uottawa.ca>
2018-12-04 13:12:48 -0500

    Most up to date file

--- app/src/main/java/com/example/maryjoe/segapp/BookService.java
+++ app/src/main/java/com/example/maryjoe/segapp/BookService.java
@@ -4,8 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class BookService extends AppCompatActivity {
+
+    public static DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+    EditText serviceNameET, serviceDateET, serviceTimeET;
+    String serviceNameS, serviceDateS, serviceTimeS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +40,29 @@ public class BookService extends AppCompatActivity {
     }
 
     public void bookButton(View v){
-        // do nothing for now
+        serviceNameET = findViewById(R.id.nameToBook);
+        serviceDateET = findViewById(R.id.dateToBook);
+        serviceTimeET = findViewById(R.id.timeToBook);
+
+        serviceNameS = serviceNameET.getText().toString().trim();
+        serviceDateS = serviceDateET.getText().toString().trim();
+        serviceTimeS = serviceTimeET.getText().toString().trim();
+
+        if (serviceNameS.isEmpty()){
+            serviceNameET.setError("Service name required");
+            serviceNameET.requestFocus();
+        }
+        if (serviceDateS.isEmpty()){
+            serviceNameET.setError("Date required");
+            serviceNameET.requestFocus();
+        }
+        if (serviceTimeS.isEmpty()){
+            serviceNameET.setError("Time required");
+            serviceNameET.requestFocus();
+        }
+
+
+
     }
 
 
