a8521d501fbad5f24033835f6111753252c95c4b
MaryJoe <msann027@uottawa.ca>
2018-12-04 20:46:16 -0500

    cirle ci test 9

--- app/src/test/java/com/example/maryjoe/segapp/AddServiceTest.java
+++ app/src/test/java/com/example/maryjoe/segapp/AddServiceTest.java
@@ -9,16 +9,14 @@ import static org.junit.Assert.*;
 public class AddServiceTest {
 
     @Test
-    public void checkEmpty() {
+    public void checkEmpty() throws ExceptionInInitializerError{
 
-        try {
             ArrayList<String> ls = new ArrayList<String>(2);
             AddService addService = new AddService(ls);
 
             boolean expected = true;
             boolean actual = addService.checkEmpty(ls);
             assertEquals(actual, expected);
-        }
-        catch (Exception e){System.out.println("Error Occured");}
+            
     }
 }
